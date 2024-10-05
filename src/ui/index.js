da6cb9f2767d119767298488615a9620a21ea726
Penguinfami <christine.spyphone.zhao@gmail.com>
2024-10-05 19:27:01 -0400

    feat: line brush tool

--- src/ui/index.js
+++ src/ui/index.js
@@ -26,7 +26,7 @@ addOnUISdk.ready.then(async () => {
   console.log(`Pixel size: ${pixelSize}, Canvas width: ${canvasWidth}`);
 
   const canvasSettings = {
-    brush: "line",
+    brush: "free",
     brushStart: [0,0],
     currentColor: "black",
     isDrawing: false,
@@ -55,7 +55,6 @@ addOnUISdk.ready.then(async () => {
       startLine(event);
     }
     else if (canvasSettings.brush == "fill") {
-        
       fill(x, y, prevColor);
     }
     canvasSettings.isDrawing = true;
@@ -67,9 +66,8 @@ addOnUISdk.ready.then(async () => {
       drawPixel(event);
   });
 
-  canvas.addEventListener("mouseup", () => {
+  canvas.addEventListener("mouseup", (event) => {
     if (canvasSettings.brush == "line") {
-      console.log("ENDING LINE")
       endLine(event);
     }
     canvasSettings.isDrawing = false;
@@ -102,39 +100,45 @@ addOnUISdk.ready.then(async () => {
 
   function startLine(event) {
     const rect = canvas.getBoundingClientRect();
-    const x = Math.floor((event.clientX - rect.left)/pixelSize) * screenScaling;
-    const y = Math.floor((event.clientY - rect.top)/pixelSize) * screenScaling;
+    const x = Math.floor((event.clientX - rect.left)/pixelSize);
+    const y = Math.floor((event.clientY - rect.top)/pixelSize);
     canvasSettings.brushStart = [x,y];
   }
 
   function endLine(event) {
     const rect = canvas.getBoundingClientRect();
-    const x = Math.floor((event.clientX - rect.left)/pixelSize) * screenScaling;
-    const y = Math.floor((event.clientY - rect.top)/pixelSize) * screenScaling;
+    const x = Math.floor((event.clientX - rect.left)/pixelSize);
+    const y = Math.floor((event.clientY - rect.top)/pixelSize);
     const startX = canvasSettings.brushStart[0];
     const startY = canvasSettings.brushStart[1];
 
-    let curX = startX;
-    let curY = startY;
+    let curX;
+    let curY;
     
-    while (curX !== x || curY !== y) {
-      console.log(`Curx: ${curX}, Cury: ${curY}`)
-      const maxDist = Math.max(Math.abs(curX - x), Math.abs(curY - y));
-      const xStep = ((x - curX)/maxDist)
-      const yStep = ((y - curY)/maxDist)
-  
-      if (curX != x)
-        curX += xStep;
-      if (curY != y)
-        curY += yStep;
-      console.log( `xStep: ${xStep} yStep: ${yStep}`)
-
-      curX = Math.floor(curX);
-      curY = Math.floor(curY);
-      
+    let fullSlope = 0;
+    if (x != startX && y != startY)
+      fullSlope = (y-startY)/(x - startX);
+    else if (x == startX)
+      fullSlope = y-startY;
+    else
+      fullSlope = 0;
+    const x0 = Math.min(startX, x);
+    const x1 = Math.max(startX, x);
+    curY = (x0 == startX ? startY : y) + 0.5;
+    for (let i = x0; i <= x1; i += 1) {
+      curX = i;
+      curY += x0 == startX ? fullSlope : fullSlope;
       context.fillStyle = canvasSettings.currentColor;
-      context.fillRect(curX*pixelSize, curY*pixelSize, pixelSize * screenScaling, pixelSize * screenScaling);
+      const drawX = curX * screenScaling;
+      for (let j = 0; j <= Math.abs(fullSlope); j++) {
+        const drawY = (Math.floor(curY -j*Math.sign(fullSlope)))
+        if (drawY >= Math.min(startY, y) && drawY <= Math.max(y, startY))
+          context.fillRect(drawX*pixelSize,drawY * screenScaling * pixelSize, pixelSize * screenScaling, pixelSize * screenScaling);    
+        else
+          break  
+      }
     }
+    
   }
 
   /*
