b7ca6318cdb20a09be66fe51e8ec9d9dd5320c76
MadisonMajarais <madison.majarais@gmail.com>
2025-02-25 01:23:08 -0500

    Add download button

--- src/ui/v2.mjs
+++ src/ui/v2.mjs
@@ -4,9 +4,9 @@ import { QuickiePixie } from "./canvas.mjs";
 addOnUISdk.ready.then(async () => {
 
     const { runtime } = addOnUISdk.instance;
-  
+
     const sandboxProxy = await runtime.apiProxy("documentSandbox");
-    
+
     const app = new QuickiePixie();
 
     app.init();
@@ -17,6 +17,21 @@ addOnUISdk.ready.then(async () => {
             .then(async blob => await sandboxProxy.createPixelImage(blob));
     });
     createImageButton.disabled = false;
+
+    const downloadImageButton = document.getElementById("downloadImage");
+    downloadImageButton.addEventListener("click", async () => {
+        const canvas = document.getElementById("pixel-canvas");
+        const link = document.createElement('a');
+        link.download = 'quickiepixie.png';
+        canvas.toBlob(async (imageBlob) => {
+            const displaylink = document.createElement('a');
+            const url = URL.createObjectURL(imageBlob);
+            displaylink.href = url;
+            displaylink.download = "quickiepixie.png"
+            displaylink.click()
+        }, 'image/png');
+    });
+    downloadImageButton.disabled = false;
 
 })
 
